fc0236d8ed4e757e6d688872579d0b4d1a746b05
Szymon Gogulski <szymongogulskiedu@gmail.com>
2026-02-20 14:00:27 +0100

    Divided panes to components. Scrollable history in progress

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,4 @@
+#include <ftxui/component/component_base.hpp>
 #include <string>
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/screen_interactive.hpp"
@@ -8,8 +9,8 @@ using namespace ftxui;
 // Wrap a component in window with a title
 Component Pane(std::string title, Component child) {
     return Renderer(child, [title, child] {
-            return window(text(title), child->Render() | flex);
-            });
+        return window(text(title), child->Render() | flex);
+    });
 }
 
 // Dropdown with a title
@@ -17,47 +18,35 @@ Component LabeledDropdown(
     std::string title,
     std::vector<std::string>* entries,
     int* selected) {
-
-  auto dropdown = Dropdown(entries, selected);
-
-  return Renderer(dropdown, [title, dropdown] {
-    return vbox({
-        text(title) | bold,
-        dropdown->Render(),
-    });
-  });
-}
-
-Component InputWithScrollableHistory() {
-
-    return Ren
-}
-
-int main() {
-    auto screen = ScreenInteractive::Fullscreen();
-
-    // Outer split state (Left/Right)
-    int lr_size = 40, lr_min = 10, lr_max = 200;
-
-    // Inner split state (Top/Bottom) 
-    int tb_size = 10, tb_min = 3, tb_max = 50;
     
-    // Left pane config menus
-    std::vector<int> entries_buadrate = {9600, 4800, 19200, 38400, 57600, 115200, 230400, 460800, 921600};
-    std::vector<std::string> entries_parity = {};
-    std::vector<std::string> entries_stop_bits = {};
-    std::vector<std::string> entries_data_bits = {};
-    std::vector<std::string> entries = {};
+    auto dropdown = Dropdown(entries, selected);
 
-    int selected_1 = 0;
-    int selected_2 = 0;
-    int selected_3 = 0;
-    int selected_4 = 0;
+    return Renderer(dropdown, [title, dropdown] {
+        return vbox({
+            text(title) | bold,
+            dropdown->Render(),
+        });
+    });
+}
+
+Component LeftMenu() {
+
+    // Left pane config menus
+    static std::vector<std::string> entries_buadrate = {"9600", "4800", "19200", "38400", "57600", "115200", "230400", "460800", "921600"};
+    static std::vector<std::string> entries_parity = {"dummy"};
+    static std::vector<std::string> entries_stop_bits = {"dummy"};
+    static std::vector<std::string> entries_data_bits = {"dummy"};
+    static std::vector<std::string> entries = {"dummy"};
+
+    static int selected_1 = 0;
+    static int selected_2 = 0;
+    static int selected_3 = 0;
+    static int selected_4 = 0;
 
     auto layout = Container::Vertical({
         Container::Horizontal({
             LabeledDropdown("COM Port", &entries, &selected_1),
-            LabeledDropdown("baudrate", &entries, &selected_2),
+            LabeledDropdown("baudrate", &entries_buadrate, &selected_2),
         }),
         Container::Horizontal({
             LabeledDropdown("Parity", &entries, &selected_3),
@@ -69,28 +58,61 @@ int main() {
         }),
     });
 
-    auto left_pane_content = Renderer(layout, [&] {
-        return layout->Render() | flex;
-    });
+    return layout;
+}
 
-    std::string input_text;
-    Component input_window = Input(&input_text, "Input text");
+Component InputWithScrollableHistory() {
+
+    static std::string input_text;
+    static std::vector<std::string> transmitted_messages;
     
     // The component tree:
-    auto input_component = Container::Vertical({
-        input_window,
+    static auto input_component = Container::Vertical({
+        Input(&input_text, "Type message and press Enter."),
     });
- 
-    // Tweak how the component tree is rendered:
-    auto input_renderer = Renderer(input_component, [&] {
-        return vbox({
-            hbox(text(" First name : "), input_component->Render()),}) | border;
+    
+    input_component = CatchEvent(input_component, [&](Event event) {
+        if (event == Event::Return && !input_text.empty()) {
+            transmitted_messages.push_back(input_text);
+            input_text.clear();
+            return true;
+        }
+        return false;
     });
 
+    
+    auto input_renderer = Renderer(input_component, [&] {
+        
+        Elements history;
+        for (const auto& msg : transmitted_messages) {
+            history.push_back(text(msg));
+        }
+
+        return vbox({
+            vbox(std::move(history)) | flex,
+            separator(),
+            hbox(text("Transmit: "), input_component->Render()),}) | border;
+    });
+    
+    return input_renderer;
+}
+
+int main() {
+    auto screen = ScreenInteractive::Fullscreen();
+
+    // Outer split state (Left/Right)
+    int lr_size = 40, lr_min = 10, lr_max = 200;
+    // Inner split state (Top/Bottom) 
+    int tb_size = 10, tb_min = 3, tb_max = 50;
+    // Left pane menu
+    auto left_menu = LeftMenu();
+    // Input menu with scrollable history
+    auto input_with_scrollable_history = InputWithScrollableHistory();
+    
     // Panes
-    auto left  = Pane("Config Menu",  left_pane_content);
+    auto left  = Pane("Config Menu",  left_menu);
     auto top   = Pane("Receiver",   Renderer([] { return text("Receiver content") | center; }));
-    auto bottom= Pane("Transmitter", input_renderer);
+    auto bottom= Pane("Transmitter", input_with_scrollable_history);
 
 
     // Right split (Top/Bottom)
@@ -103,8 +125,6 @@ int main() {
         .max = &tb_max,
     });
 
-
-
     // Outer split (Left/Right)
     auto outer = ResizableSplit({
         .main = left,
@@ -114,7 +134,6 @@ int main() {
         .min = &lr_min,
         .max = &lr_max,
     });
-
 
     auto app = Renderer(outer, [&] {
         return outer->Render();
